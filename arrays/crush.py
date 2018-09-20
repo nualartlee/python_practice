93c35a10d8644193ce8c33339fe5f246dc17706b
Javier Nualart Lee <javier@plc.ninja>
2018-09-20 10:11:15 -0700

    First attempt at crush.
    On branch master
    Changes committed:
    
    	modified:   crush.py

--- arrays/crush.py
+++ arrays/crush.py
@@ -5,26 +5,39 @@ import os
 import random
 import re
 import sys
+import fileinput
+
 
 # Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
+    arr = [0 for _ in range(n)]
+    for query in queries:
+        new_arr = arr[:query[0]]
+        new_arr += map(lambda x: x + query[2], arr[query[0]:query[1]+1])
+        new_arr += arr[query[1] + 1:]
+        arr = new_arr
+    return max(arr)
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    nm = input().split()
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    #nm = input().split()
 
-    n = int(nm[0])
+    nm = fileinput.input()
+    line1 = nm[0].split()
 
-    m = int(nm[1])
+    n = int(line1[0])
+
+    m = int(line1[1])
 
     queries = []
 
-    for _ in range(m):
-        queries.append(list(map(int, input().rstrip().split())))
+    for i in range(m):
+        queries.append(list(map(int, nm[i+1].rstrip().split())))
 
     result = arrayManipulation(n, queries)
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(result)
+    #fptr.write(str(result) + '\n')
+    #fptr.close()
