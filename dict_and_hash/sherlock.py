9d0d26aaed7cb7dc7f6b876339b0f987345a0c9b
Javier Nualart Lee <javier@plc.ninja>
2018-09-23 21:43:25 -0700

    Sherlock completed.
    On branch master
    Changes committed:
    
    	modified:   sherlock.py

--- dict_and_hash/sherlock.py
+++ dict_and_hash/sherlock.py
@@ -19,20 +19,17 @@ def sherlockAndAnagrams(s):
 
         # For each subset of i characters starting at j
         for j in range(len(s_list) - i):
+            sublist = sorted(s_list[j:j + i])
+            sublist_set = set(sublist)
 
             # Check if the subset exists in the remaining string
-            if not set(s_list[j:j + i]).issubset(set(s_list[j+1:])):
-                continue
+            if sublist_set.issubset(set(s_list[j+1:])):
 
-            # For each subset of characters starting at k
-            for k in range(j + 1, len(s_list) - i + 1):
+                # For each subset of characters starting at k
+                for k in range(j + 1, len(s_list) - i + 1):
 
-                # Add if there is a match
-                result += sorted(s_list[j:j + i]) == sorted(s_list[k:k + i])
-
-                # Check if the subset exists in the remaining string
-                if not set(s_list[j:j + i]).issubset(set(s_list[k+1:])):
-                    break
+                    # Add if there is a match
+                    result += sublist == sorted(s_list[k:k + i])
 
     return result
 
@@ -48,15 +45,15 @@ if __name__ == '__main__':
         print(result)
     print(datetime.now() - start)
 
-    # 100: 807
-    s = 'shdfuytwqeuigvsdfugshjdvcjhsgyuftweiufjkxvgxhgviusgfsiudwggfiusytiwehjvcsjgeyrgefhwegwfgefvjshdjsadn'
-    print(len(s))
-    start = datetime.now()
+    ## 100: 807
+    #s = 'shdfuytwqeuigvsdfugshjdvcjhsgyuftweiufjkxvgxhgviusgfsiudwggfiusytiwehjvcsjgeyrgefhwegwfgefvjshdjsadn'
+    #print(len(s))
+    #start = datetime.now()
     #print(sherlockAndAnagrams(s))
-    print(datetime.now() - start)
-    # 150: 1485
-    s = 'shdfuytwqeuigvsdfosaiiutixviuwxneyrrcweolxzwoeiuycitbxweyrcybufwgxyugshjdvcjhsgyuftweiufjkxvgxhgviusgfsiudwggfiusytiwehjvcsjgeyrgefhwegwfgefvjshdjsadn'
-    print(len(s))
-    start = datetime.now()
+    #print(datetime.now() - start)
+    ## 150: 1485
+    #s = 'shdfuytwqeuigvsdfosaiiutixviuwxneyrrcweolxzwoeiuycitbxweyrcybufwgxyugshjdvcjhsgyuftweiufjkxvgxhgviusgfsiudwggfiusytiwehjvcsjgeyrgefhwegwfgefvjshdjsadn'
+    #print(len(s))
+    #start = datetime.now()
     #print(sherlockAndAnagrams(s))
-    print(datetime.now() - start)
+    #print(datetime.now() - start)
